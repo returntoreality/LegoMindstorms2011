8a7125bf35ee38c7ee66f39b7589858520ed4857
Martin Keßler <martin@martin-laptop.(none)>
2011-03-07 12:34:33 +0100

    Zielvorstellung

--- SkyNet/SkyNet.java
+++ SkyNet/SkyNet.java
@@ -1,23 +1,13 @@
 import lejos.nxt.*;
-/**
- * @author Linus Lotz
- * 
- */
 
+public class SkyNet {
 
-public class SkyNet
-{
-
-	
-  public static void main (String[] aArg)
-  throws Exception
-  {
-	  LCD.drawString("ALARM!!!!", 0, 0);
-	  while(true)
-	  {
-		  Sound.setVolume(100);
-		  Sound.buzz();
-		  Thread.sleep(100);
-	  }
-  }
+	public static void main(String[] aArg) throws Exception {
+		LCD.drawString("ALARM!!!!", 0, 0);
+		while (true) {
+			Sound.setVolume(100);
+			Sound.buzz();
+			Thread.sleep(100);
+		}
+	}
 }
